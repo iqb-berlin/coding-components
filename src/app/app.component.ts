49ca6154293a7d8660340558a1b96f237a9162af
mechtelm <mechtelqiqb.hu-berlin.de>
2023-09-06 18:17:42 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,70 +1,10 @@
-import {Component} from '@angular/core';
-import {CodingScheme, VariableCodingData, VariableInfo} from "@iqb/responses";
-import sampleVarList1 from '../../sample-data/var-list-1.json';
-import sampleCodings1 from '../../sample-data/coding-scheme-1.json';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-      <mat-drawer-container class="coder-body">
-        <mat-drawer #drawer mode="side">
-            <schema-checker [codingScheme]="codings1"></schema-checker>
-        </mat-drawer>
-        <mat-drawer-content class="drawer-content">
-          <div>
-            <button mat-icon-button (click)="drawer.toggle()" [matTooltip]="drawer.opened ? 'Check ausblenden' : 'Check einblenden'">
-              <mat-icon>{{drawer.opened ? 'chevron_left' : 'chevron_right'}}</mat-icon>
-            </button>
-          </div>
-          <iqb-schemer class="drawer-schemer"
-                  [varList]="varList1"
-                  [codingScheme]="codings1"
-          ></iqb-schemer>
-        </mat-drawer-content>
-      </mat-drawer-container>
-      <schemer-load-save [varList]="varList1" [codingScheme]="codings1" [style.height.px]="0"></schemer-load-save>
-  `,
-    styles: [
-      `
-        .coder-body {
-          position: absolute;
-          width: 100%;
-          height: 100%;
-          top: 0;
-          bottom: 0;
-          padding: 0;
-          margin: 0;
-          flex-direction: row;
-          justify-content: space-between;
-          align-items: stretch;
-        }
-      `,
-      `
-        .drawer-button {
-          flex: 0 0 40px;
-        }
-      `,
-      `
-        .drawer-schemer {
-          flex: 1 1 auto;
-        }
-      `,
-      `
-        .drawer-content {
-          padding: 0;
-          display: flex;
-          flex-direction: row;
-          justify-content: space-between;
-          align-items: stretch;
-          overflow: unset;
-        }
-      `
-    ]
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  varList1 = sampleVarList1 as VariableInfo[];
-  codings1 = new CodingScheme(sampleCodings1 as VariableCodingData[]);
   title = 'coding-components';
-
-  constructor() {  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,29 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NgxCodingComponentsModule } from 'ngx-coding-components';
-import {MatMenuModule} from "@angular/material/menu";
-import {MatDividerModule} from "@angular/material/divider";
-import {MatIconModule} from "@angular/material/icon";
-import {MatButtonModule} from "@angular/material/button";
-import {MatTooltipModule} from "@angular/material/tooltip";
-import {MatSidenavModule} from "@angular/material/sidenav";
-import {MatToolbarModule} from "@angular/material/toolbar";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    NgxCodingComponentsModule,
-    MatMenuModule,
-    MatDividerModule,
-    MatIconModule,
-    MatButtonModule,
-    MatTooltipModule,
-    MatSidenavModule,
-    MatToolbarModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
