02cd1eebfa89c1cbd1e453c811f949deefbad56a
mechtelm <mechtelqiqb.hu-berlin.de>
2023-09-07 18:45:12 +0200

    Fix element build

--- src/app/element.module.ts
+++ src/app/element.module.ts
@@ -1,4 +1,26 @@
-import {NgModule} from "@angular/core";
+import {ApplicationRef, DoBootstrap, Injector, NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+import {SchemerModule} from "./schemer/schemer.module";
+import {createCustomElement} from "@angular/elements";
+import {SchemerComponent} from "./schemer/schemer.component";
 
-@NgModule()
-export class ElementModule { }
+@NgModule({
+    imports: [
+        BrowserModule,
+        SchemerModule
+    ],
+    providers: []
+})
+export class ElementModule implements DoBootstrap {
+    constructor(
+        private injector: Injector
+    ) {}
+    ngDoBootstrap(appRef: ApplicationRef) {
+        if (!customElements.get('login-provider')) {
+            const schemerComponent = createCustomElement(SchemerComponent, {
+                injector: this.injector,    // This injector is used to load the component's factory
+            });
+            customElements.define('responses-schemer', schemerComponent);
+        }
+    }
+}
