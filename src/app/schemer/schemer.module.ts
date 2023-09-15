f87788414d04fe54c0cba70260a48f672f6ad82a
mechtelm <mechtel@iqb.hu-berlin.de>
2023-09-15 15:43:33 +0200

    Chaotic code 2

--- src/app/schemer/schemer.module.ts
+++ src/app/schemer/schemer.module.ts
@@ -3,13 +3,24 @@ import { CommonModule } from '@angular/common';
 import { SchemerComponent } from './schemer.component';
 import { NgxTiptapModule } from 'ngx-tiptap';
 import {MatSelectModule} from "@angular/material/select";
-import {ReactiveFormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatMenuModule} from "@angular/material/menu";
 import {MatButtonModule} from "@angular/material/button";
 import {RichTextEditDialogComponent} from "./rich-text-editor/rich-text-edit-dialog.component";
 import {RichTextEditorComponent} from "./rich-text-editor/rich-text-editor.component";
 import {MatDialogModule} from "@angular/material/dialog";
+import {MatIconModule} from "@angular/material/icon";
+import {MatTooltipModule} from "@angular/material/tooltip";
+import {MatInputModule} from "@angular/material/input";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatCardModule} from "@angular/material/card";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatListModule} from "@angular/material/list";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatChipsModule} from "@angular/material/chips";
+import {BrowserModule} from "@angular/platform-browser";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 
 
@@ -24,13 +35,25 @@ import {MatDialogModule} from "@angular/material/dialog";
   ],
   imports: [
     CommonModule,
-    NgxTiptapModule,
-    MatSelectModule,
-    ReactiveFormsModule,
-    MatFormFieldModule,
-    MatMenuModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
     MatButtonModule,
-    MatDialogModule
+    MatIconModule,
+    MatTooltipModule,
+    MatListModule,
+    MatMenuModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    FormsModule,
+    MatSelectModule,
+    MatChipsModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatSidenavModule,
+    NgxTiptapModule
   ]
 })
 export class SchemerModule { }
