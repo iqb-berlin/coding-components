5e423229171abfcf1d71fd37c84923228e589212
mechtelm <mechtel@iqb.hu-berlin.de>
2023-09-19 17:39:58 +0200

    Add translation service

--- src/app/schemer/app-translate-loader.ts
+++ src/app/schemer/app-translate-loader.ts
@@ -1,11 +1,14 @@
 import { TranslateLoader } from '@ngx-translate/core';
-import { from, Observable } from 'rxjs';
+import {from, merge, Observable} from 'rxjs';
 import { reduce } from 'rxjs/operators';
 
 export class AppTranslateLoader implements TranslateLoader {
   // eslint-disable-next-line class-methods-use-this
-  getTranslation = (lang: string): Observable<Record<string, string>> =>
-    from(import(`../../assets/i18n/${lang}.json`))
+  getTranslation = (lang: string): Observable<Record<string, string>> => merge(
+    from(import(`../../assets/i18n/${lang}.json`)),
+    from(import(`../../assets/i18n/${lang}2.json`)),
+    from(import(`../../assets/i18n/${lang}3.json`))
+  )
   .pipe(
     reduce((
       merged: Record<string, string>,
