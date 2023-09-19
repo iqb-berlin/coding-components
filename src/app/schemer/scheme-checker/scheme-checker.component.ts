7b036cdf648453698b80050e7ca79d756582a354
mechtelm <mechtel@iqb.hu-berlin.de>
2023-09-19 12:23:29 +0200

    Add checker

--- src/app/schemer/scheme-checker/scheme-checker.component.ts
+++ src/app/schemer/scheme-checker/scheme-checker.component.ts
@@ -1,6 +1,6 @@
-import {Component, Inject} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {Response, ResponseScheme} from '@iqb/responses';
-import {MAT_DIALOG_DATA, MatDialog} from '@angular/material/dialog';
+import {MatDialog} from '@angular/material/dialog';
 import { ShowCodingResultsComponent } from './show-coding-results.component';
 
 @Component({
@@ -10,41 +10,58 @@ import { ShowCodingResultsComponent } from './show-coding-results.component';
 })
 export class SchemeCheckerComponent {
   values: { [Key in string]: string } = {};
+  _responseScheme: ResponseScheme | null = null;
+  @Input()
+  set responseScheme(value: any) {
+    this.values = {};
+    this._responseScheme = null;
+    if (value) {
+      if (typeof value === 'string') {
+        this._responseScheme = JSON.parse(value);
+      } else {
+        this._responseScheme = value;
+      }
+      if (this._responseScheme) {
+        this._responseScheme.variableCodings.filter(v => v.sourceType === 'BASE')
+          .map(v => v.id)
+          .sort()
+          .forEach(v => this.values[v] = '')
+      }
+    }
+  }
 
   constructor(
-    private showCodingResultsDialog: MatDialog,
-    @Inject(MAT_DIALOG_DATA) public data: ResponseScheme
-  ) { }
+    private showCodingResultsDialog: MatDialog
+  ){}
 
-  getBaseVariableIds(): string[] {
-    return this.data.variableCodings.filter(v => v.sourceType === 'BASE')
-        .map(v => v.id)
-        .sort()
-  }
   updateInputValue(sourceElement: EventTarget | null, targetVariable: string) {
     if (sourceElement) this.values[targetVariable] = (sourceElement as HTMLInputElement).value;
   }
 
   startEvaluation() {
-    const myValues: Response[] = [];
-    this.data.variableCodings.forEach((cs: { id: string | number; }) => {
-      if (this.values[cs.id]) {
-        myValues.push(<Response>{
-          id: cs.id,
-          value: this.values[cs.id],
-          status: 'VALUE_CHANGED'
-        });
-      } else {
-        myValues.push(<Response>{
-          id: cs.id,
-          value: null,
-          status: 'UNSET'
-        });
-      }
-    });
-    this.showCodingResultsDialog.open(ShowCodingResultsComponent, {
-      width: '800px',
-      data: this.data.codeAndScore(myValues)
-    });
+    if (this._responseScheme) {
+      const myValues: Response[] = [];
+      this._responseScheme.variableCodings.forEach((cs: { id: string | number; }) => {
+        if (this.values[cs.id]) {
+          myValues.push(<Response>{
+            id: cs.id,
+            value: this.values[cs.id],
+            status: 'VALUE_CHANGED'
+          });
+        } else {
+          myValues.push(<Response>{
+            id: cs.id,
+            value: null,
+            status: 'UNSET'
+          });
+        }
+      });
+      this.showCodingResultsDialog.open(ShowCodingResultsComponent, {
+        width: '800px',
+        data: this._responseScheme.codeAndScore(myValues)
+      });
+    }
   }
+
+  protected readonly Object = Object;
 }
