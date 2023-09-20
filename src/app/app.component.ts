bd22a786808bd771efcc623fdcefc33dcc4db598
mechtelm <mechtel@iqb.hu-berlin.de>
2023-09-20 12:22:05 +0200

    Apply material theming

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import {VariableInfo} from "@iqb/responses";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+      <iqb-schemer
+              [varList]="sampleVarList"
+              [showChecker]="false"
+              [showFileMenu]="true"
+      ></iqb-schemer>
+  `
 })
 export class AppComponent {
   title = 'coding-components';
