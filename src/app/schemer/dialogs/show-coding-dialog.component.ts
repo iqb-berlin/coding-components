00d7e9898b3d9f9280c01d54d6daf022f662c126
mechtelm <mechtelqiqb.hu-berlin.de>
2023-11-22 21:14:47 +0100

    Fix layout in some coding models

---
+++ src/app/schemer/dialogs/show-coding-dialog.component.ts
@@ -0,0 +1,63 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import {CodingAsText, VariableCodingData} from "@iqb/responses";
+import {CodingFactory} from "@iqb/responses/coding-factory";
+
+@Component({
+  template: `
+    <h1 mat-dialog-title>{{varCoding.id}}{{varCoding.label ? ' - ' + varCoding.label : ''}}</h1>
+
+    <mat-dialog-content>
+      <div class="fx-row-start-center">
+        <div class="fx-flex-row-20">Quelle:</div>
+        <div class="fx-flex-fill">{{varCodingText.source}}</div>
+      </div>
+      <div class="fx-row-start-center">
+        <div class="fx-flex-row-20">Verarbeitung:</div>
+        <div class="fx-flex-fill">{{varCodingText.processing || 'keine'}}</div>
+      </div>
+      <div class="fx-row-start-center">
+        <div class="fx-flex-row-20">Allgemeine Instruktion:</div>
+        <div class="fx-flex-fill">{{varCodingText.hasManualInstruction ? 'ja' : 'keine'}}</div>
+      </div>
+      <h3>Codes</h3>
+      <ng-container *ngIf="varCodingText.codes.length > 0">
+        <div class="fx-row-start-center" [style.font-weight]="'bold'">
+          <div class="fx-flex-row-10" [style.text-align]="'center'">Code</div>
+          <div class="fx-flex-row-10" [style.text-align]="'center'">Score</div>
+          <div class="fx-flex-row-20">Titel</div>
+          <div class="fx-flex-fill">Bedingung(en)</div>
+          <div class="fx-flex-row-10" [style.text-align]="'center'">Instruktions- text ?</div>
+        </div>
+        <div class="fx-row-start-center code-row" *ngFor="let codeText of varCodingText.codes">
+          <div class="fx-flex-row-10" [style.text-align]="'center'">{{codeText.code}}</div>
+          <div class="fx-flex-row-10" [style.text-align]="'center'">{{codeText.score}}</div>
+          <div class="fx-flex-row-20">{{codeText.label}}</div>
+          <div class="fx-flex-fill">{{codeText.description || 'keine'}}</div>
+          <div class="fx-flex-row-10" [style.text-align]="'center'">{{codeText.hasManualInstruction ? 'ja' : '-'}}</div>
+        </div>
+      </ng-container>
+      <div *ngIf="varCodingText.codes.length === 0">keine</div>
+    </mat-dialog-content>
+
+    <mat-dialog-actions>
+      <button mat-raised-button [mat-dialog-close]="false">{{'dialog-close' | translate}}</button>
+    </mat-dialog-actions>
+  `,
+  styles: [
+    '.code-row:nth-child(even) {background-color: #f1f1f1;}'
+  ]})
+export class ShowCodingDialogComponent {
+  varCodingText: CodingAsText;
+
+  constructor(@Inject(MAT_DIALOG_DATA) public varCoding: VariableCodingData) {
+    this.varCodingText = {
+      id: this.varCoding.id,
+      label: this.varCoding.label,
+      source: CodingFactory.sourceAsText(this.varCoding.id, this.varCoding.sourceType, this.varCoding.deriveSources),
+      processing: CodingFactory.processingAsText(this.varCoding.processing),
+      hasManualInstruction: !!this.varCoding.manualInstruction,
+      codes: this.varCoding.codes.map(code => CodingFactory.codeAsText(code))
+    };
+  }
+}
