986a88ee86d44330939f927a175141f8318f52ae
mechtelm <mechtelqiqb.hu-berlin.de>
2023-12-02 22:56:24 +0100

    Fix merge damage ngx-coding-components

--- projects/coding-components/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of coding-components
- */
-
-export * from './lib/coding-components.service';
-export * from './lib/coding-components.component';
-export * from './lib/coding-components.module';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {MatMenuModule} from "@angular/material/menu";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {MatTooltipModule} from "@angular/material/tooltip";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatToolbarModule} from "@angular/material/toolbar";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxCodingComponentsModule,
+    MatMenuModule,
+    MatDividerModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatSidenavModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
