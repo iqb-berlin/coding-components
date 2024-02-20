d6e353c9d7ed9543ba56b75a095f2e307b45dd0a
jurei733 <67505990+jurei733@users.noreply.github.com>
2024-02-20 14:20:06 +0100

    Update show-coding-results.component.ts

--- projects/ngx-coding-components/src/lib/scheme-checker/show-coding-results.component.ts
+++ projects/ngx-coding-components/src/lib/scheme-checker/show-coding-results.component.ts
@@ -4,38 +4,37 @@ import { Response } from '@iqb/responses';
 
 @Component({
   template: `
-      <h1 mat-dialog-title>{{'coding.result-header' | translate}}</h1>
+      <h1 mat-dialog-title>{{'coding.result' | translate}}</h1>
 
       <mat-dialog-content>
         <div class="response-list">
-          <div class="response-row" [style.grid-template-columns]="'2fr 5fr 2fr 1fr 1fr'">
-            <div>Variable</div>
-            <div>Wert</div>
-            <div>Status</div>
-            <div class="centered-column">Code</div>
-            <div class="centered-column">Bew.</div>
-          </div>
-          <div class="response-row"
-               [style.grid-template-columns]="'2fr 5fr 2fr 1fr 1fr'"
-               *ngFor="let rd of data">
-            <div>{{rd.id}}</div>
-            <div>{{rd.value}}</div>
-            <div>{{rd.status}}: {{'status-label.' + rd.status | translate}}</div>
-            <div class="centered-column">{{rd.code || rd.code === 0 ? rd.code : '-'}}</div>
-            <div class="centered-column">{{rd.score || rd.score === 0 ? rd.score : '-'}}</div>
-          </div>
+            <span class="column-header">{{'coding.variable' | translate}}</span>
+            <span class="column-header">{{'coding.value' | translate }}</span>
+            <span class="column-header">{{'coding.state' | translate }}</span>
+            <span class="column-header centered-column">{{'coding.code' | translate }}</span>
+            <span class="column-header centered-column">{{'coding.score' | translate }}</span>
+          <ng-container  *ngFor="let rd of data">
+              <span class="column-row">{{rd.id}}</span>
+              <span class="column-row">{{rd.value}}</span>
+              <span class="column-row">{{rd.status}}: {{'status-label.' + rd.status | translate}}</span>
+              <span class="column-row centered-column">{{rd.code || rd.code === 0 ? rd.code : '-'}}</span>
+              <span class="column-row centered-column">{{rd.score || rd.score === 0 ? rd.score : '-'}}</span>
+          </ng-container>
         </div>
       </mat-dialog-content>
 
-      <mat-dialog-actions>
-        <button mat-raised-button [mat-dialog-close]="false">{{'dialog-close' | translate}}</button>
+      <mat-dialog-actions align="end">
+        <button mat-raised-button
+                color="primary"
+                [mat-dialog-close]="false">
+                {{'close' | translate}}
+        </button>
       </mat-dialog-actions>
     `,
   styles: [
-    '.response-list .response-row {display: grid; grid-template-rows: auto; column-gap: 10px}',
-    '.response-list .response-row:nth-child(even) {background-color: #f1f1f1;}',
-    '.centered-column {text-align: center}'
-  ]
+    '.response-list  {display: grid; grid-template-columns: 2fr 4fr 2fr 1fr 1fr ; column-gap: 10px; row-gap: 10px;}',
+    '.centered-column {text-align: center}',
+    '.column-header {font-weight: bold; position: sticky; top: 0; background: white; padding:10px 0}']
 })
 export class ShowCodingResultsComponent {
   constructor(
