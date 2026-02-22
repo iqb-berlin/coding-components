4fe4d2149ff19c08a57a30b03c6fc5faf482c55b
jurei733 <67505990+jurei733@users.noreply.github.com>
2026-02-22 12:19:15 +0100

    Revert "Remove Zone.js dependencies and enable zoneless change detection. Cleanup related configurations and update build tools."
    This reverts commit fd5bda621dcb9845c8a7fc9af120ba0f36a2c316.

--- projects/schemer/src/main.ts
+++ projects/schemer/src/main.ts
@@ -1,4 +1,5 @@
-import { enableProdMode, importProvidersFrom, provideZonelessChangeDetection } from '@angular/core';
+import 'zone.js';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { createApplication } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 import { createCustomElement } from '@angular/elements';
@@ -16,7 +17,6 @@ if (environment.production) {
 (async () => {
   const app = await createApplication({
     providers: [
-      provideZonelessChangeDetection(),
       provideAnimations(),
       provideHttpClient(),
       importProvidersFrom(
