989618dfb4a7fbe9d5cbe30a9425e8de7dc91a7e
jurei733 <67505990+jurei733@users.noreply.github.com>
2026-06-16 14:39:29 +0200

    Add codebook export entry point

---
+++ projects/ngx-coding-components/codebook-export/src/public-api.ts
@@ -0,0 +1,9 @@
+export * from './lib/codebook-export/codebook-export.component';
+export * from './lib/codebook-export/codebook-export.provider';
+export type {
+  CodeBookContentSetting,
+  CodebookExportConfig,
+  Missing,
+  MissingsProfile,
+  UnitSelectionItem
+} from '@iqb/ngx-coding-components/codebook-models';
